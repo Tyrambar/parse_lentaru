3d428922131bf7312b09e15d07d6fdfcbe6554d5
Ubuntu <ubuntu@ip-172-31-8-195.eu-north-1.compute.internal>
2020-02-25 01:09:58 +0000

    third commit

--- lenta_ru.py
+++ lenta_ru.py
@@ -23,7 +23,7 @@ ART_TAGS = {
 
 
 class Art(NamedTuple):
-    rubric : int
+    rubric : str
     title : str
     link : str
     date : datetime
@@ -40,7 +40,7 @@ def createParser():
 
     return parser
 
-def to_format_date(raw_date):
+def to_format_date(raw_date, dat_url):
     if raw_date[5] != ' ':
         raw_date = raw_date[:5] + ' ' + raw_date[5:]
     date_spl = re.sub('—', '', raw_date).split()
@@ -48,7 +48,7 @@ def to_format_date(raw_date):
         format_date = ' '.join(date_spl[:-1]) + \
                       datetime.strftime(datetime.today(), ' %d %m %Y')
     else:
-        if date_url[:4] != '2020':
+        if dat_url[:4] != '2020':
             format_date = ' '.join(date_spl[:-2] + [MONTHS[date_spl[-2]]] + [date_spl[-1]])
         else:
             format_date = ' '.join(date_spl[:-1] + [MONTHS[date_spl[-1]]] +
@@ -58,15 +58,21 @@ def to_format_date(raw_date):
 
 def get_art_attrs(rubric_url, date_url, dat_key):
     global all_art
+    print('begin get', date_url)
     req = requests.get(f'{MAIN_URL}/{rubric_url}/{date_url}', headers=USERAGENT)
+    print(req.text)
+    if 'div' in req.text:
+        print('diiiv')
     bs = Bs(req.text, "html.parser")
     if rubric_url == 'all':
-        rubrics = (ART_TAGS['articles'], ART_TAGS['news'])
+        rubrics = ('articles', 'news')
     else:
-        rubrics = (ART_TAGS[rubric_url], )
+        rubrics = (rubric_url, )
     count_art_per_date = 0
     for rubric in rubrics:
-        all_raw_art = bs.find_all("div", class_=rubric)
+        print('circle', rubric)
+        all_raw_art = bs.find_all("div", class_=ART_TAGS[rubric])
+        print(ART_TAGS[rubric], all_raw_art)
         count_art_per_date += len(all_raw_art)
         if dat_key in all_art.keys():
             if count_art_per_date == len(all_art[dat_key]):
@@ -75,11 +81,12 @@ def get_art_attrs(rubric_url, date_url, dat_key):
             date = raw_art.find("span", class_='g-date item__date').get_text()
             title_art = raw_art.find("div", class_="titles").find('a')
             art = Art(
-                rubric = rubric_url,
+                rubric = rubric,
                 title = title_art.get_text(),
                 link = title_art.get('href'),
-                date = datetime.strptime(to_format_date(date), '%H:%M %d %m %Y')
+                date = datetime.strptime(to_format_date(date, date_url), '%H:%M %d %m %Y')
             )
+            print('added art')
             if dat_key not in all_art.keys():
                 all_art[dat_key] = [art, ]
             else:
@@ -91,6 +98,7 @@ def main():
     global all_art
     path_file = namespace.file[1:-1]
     rubric_url = namespace.rubric if namespace.rubric else 'all'
+    print('in main', rubric_url)
     if os.path.exists('/'.join(path_file.split('/')[:-1])):
         try:
             with open(path_file, 'rb') as art_pkl:
@@ -102,13 +110,17 @@ def main():
 
     if namespace.date:
         date_url = '/'.join(namespace.date.split('.'))
-        date_key = datetime.strptime(namespace.date, '%Y.%m.%d')
+        date_key = datetime.strptime(namespace.date, '%Y.%m.%d').date()
+        print(date_key)
         get_art_attrs(rubric_url, date_url, date_key)
     else:
         date_url = datetime.strftime(datetime.today(), '%Y/%m/')
+        print(date_url)
         for date_month in range(1, int(datetime.today().day)+1):
-            date_key = datetime.strptime(date_url + str(date_month), '%Y/%m/%d')
-            get_art_attrs(rubric_url, date_url + str(date_month), date_key)
+            date_month_str = f'{"0" if date_month < 10 else ""}{str(date_month)}'
+            date_key = datetime.strptime(date_url + date_month_str, '%Y/%m/%d').date()
+            print('in date_month', date_key)
+            get_art_attrs(rubric_url, date_url + date_month_str, date_key)
 
     with open(path_file, 'wb') as art_pkl:
         pickle.dump(all_art, art_pkl)
